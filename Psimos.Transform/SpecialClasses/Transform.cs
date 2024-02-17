b0573b9e8167d46c2d757ce14e8e77f2a6290afe
ShirokoChanDesuyo~ <73055129+ShenWeiqi0804@users.noreply.github.com>
2024-02-17 17:30:35 +0800

    Add files via upload

---
+++ Psimos.Transform/SpecialClasses/Transform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psimos.Transform.SpecialClasses
+{
+    public class Transform : Support.Behaviour
+    {
+        public Vector2D rotation, position, scale;
+    }
+}
---
+++ Psimos.Transform/SpecialClasses/Vector2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psimos.Transform.SpecialClasses
+{
+    public struct Vector2D
+    {
+        public float x, y;
+        public Vector2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2D Normalize()
+        {
+            if (x == 0 && y == 0) return this;
+            float r = (float)Math.Sqrt(x * x + y * y);
+            return new Vector2D(x / r, y / r);
+        }
+        public static Vector2D Zero { get { return new Vector2D(0, 0); } }
+
+    }
+}
